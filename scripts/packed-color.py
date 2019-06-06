93c0e340f25cd643d87bf13db41a944b31dca714
lzmunch <lauren.zll.zhang@gmail.com>
2019-06-06 16:07:45 -0400

    added general packed color script

---
+++ scripts/packed-color.py
@@ -0,0 +1,20 @@
+# packed-color.py
+# turns hex string color into packed color (int)
+import matplotlib.colors as mp_colors
+
+hexArr = ["#52b947", "#f3ec19", "#f57e20","#ed1f24","#991b4f"]
+
+def hex_to_rgb(hex_string):
+   rgb = mp_colors.hex2color(hex_string)
+   r,g,b = tuple([int(255*x) for x in rgb])
+   return (r,g,b)
+
+def rgb_to_pack_color(color):
+   return color[0] + color[1] * 256.0 + color[2] * 256.0 * 256.0;
+
+def hex_to_pack_color(hex_string):
+  return rgb_to_pack_color(hex_to_rgb(hex_string))
+
+# convert all colors in hexArr to packed colors
+for i in range(len(hexArr)):
+  print(hex_to_pack_color(hexArr[i]))
