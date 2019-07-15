7b266e24165fdb778b96ac5894674632a4d39179
Paul Dille <pdille@cmucreatelab.org>
2019-07-14 20:16:53 -0400

    - Add javascript varient of hex to packedColor - Minor tweaks/cleanup

---
+++ scripts/README.txt
@@ -0,0 +1,1 @@
+Go to http://htmlpreview.github.io/?https://github.com/CMU-CREATE-Lab/making-earthtime-maps/blob/master/scripts/index.html to view as a webpage
---
+++ scripts/script.js
@@ -0,0 +1,30 @@
+function hexToRgb(hex) {
+  // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+  var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+  hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+    return r + r + g + g + b + b;
+  });
+
+  var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16)
+  } : null;
+}
+
+function rgbToHex(r, g, b) {
+  return "#" + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+function hexToPackedColor(hex) {
+  var inputHex = document.getElementById("hex-input").value || hex;
+  var c = hexToRgb(inputHex);
+  var packedColor = parseFloat(c.r + c.g * 256 + c.b * 256.0 * 256.0).toFixed(1);
+  var packedColorDomField = document.getElementById("packedColor");
+  if (packedColorDomField) {
+    packedColorDomField.innerHTML = packedColor;
+  } else {
+    return packedColor;
+  }
+}
